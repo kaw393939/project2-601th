7cc958ecbd0bb914969488fbca47e0e87ed11be7
kaw393939 <kwilliam@njit.edu>
2025-01-30 14:58:00 -0500

    changed github action coverage requirement to 90 percent

--- app/main.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print('hello')
-
-
-if __name__ == "__main__":
-    # Now, we use the calculator tool we got earlier. This will start the calculator, which is a program 
-    # that keeps running and doing math based on what we tell it.
-    main()
